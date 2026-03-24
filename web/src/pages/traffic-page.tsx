1e73e3a30b4ff91a36bf5d47edfec597322b2d5f
Steven Normore <snormore@gmail.com>
2026-03-24 11:14:06 -0400

    web: make interfaces page full width

--- web/src/pages/traffic-page.tsx
+++ web/src/pages/traffic-page.tsx
@@ -413,7 +413,7 @@ function TrafficPageContent() {
 
   return (
     <div className="flex-1 overflow-auto">
-      <div className="max-w-7xl mx-auto px-4 sm:px-8 py-8">
+      <div className="px-4 sm:px-8 py-8">
         <PageHeader
           icon={Network}
           title="Interfaces"
